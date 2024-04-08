b190d960eba1f6f7ef4e8a1422ef486ebfa3a870
DoDesCadeaux <dduraku@student.s19.be>
2024-04-08 23:59:46 +0200

    packer changes

--- lua/base/packer.lua
+++ lua/base/packer.lua
@@ -12,53 +12,64 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	{
-	  "olimorris/onedarkpro.nvim",
-	  priority = 1000, -- Ensure it loads first
-	},
-	{ 'wakatime/vim-wakatime', lazy = false },
-	'nvim-tree/nvim-web-devicons',
-	'folke/neodev.nvim',
-	"nvim-lua/plenary.nvim",
-	'nvim-treesitter/nvim-treesitter-context',
-	'nvim-treesitter/playground',
-	'mbbill/undotree',
-	'neovim/nvim-lspconfig',
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
-	'hrsh7th/nvim-cmp',
-	'hrsh7th/cmp-nvim-lsp',
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		dependencies = { {'nvim-lua/plenary.nvim'} }
-	},
-	{
-		'kylechui/nvim-surround',
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-		require("nvim-surround").setup({})
-		end
-	},
-	{ 'nvim-treesitter/nvim-treesitter'},
-	{
-		'windwp/nvim-autopairs',
-		config = function() require("nvim-autopairs").setup ({}) end
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+  },
+  { 'wakatime/vim-wakatime', lazy = false },
+  'nvim-tree/nvim-web-devicons',
+  'folke/neodev.nvim',
+  "nvim-lua/plenary.nvim",
+  'nvim-treesitter/nvim-treesitter-context',
+  'nvim-treesitter/playground',
+  'mbbill/undotree',
+  'neovim/nvim-lspconfig',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    -- or                            , branch = '0.1.x',
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  {
+    'kylechui/nvim-surround',
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  },
+  { 'nvim-treesitter/nvim-treesitter'},
+  {
+    'windwp/nvim-autopairs',
+    config = function() require("nvim-autopairs").setup ({}) end
+  },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'}, -- Required
-			{'saadparwaiz1/cmp_luasnip'},
-		}
-	},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'}, -- Required
+      {'saadparwaiz1/cmp_luasnip'},
+    }
+  },
 })
+
+-- Configure treesitter for Python syntax highlighting
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "python" }, -- Install the language if not available
+  highlight = {
+    enable = true,              -- Enable treesitter
+    disable = {},               -- Disable tree-sitter for other languages
+  },
+}
+
+require'lspconfig'.pyright.setup{}
