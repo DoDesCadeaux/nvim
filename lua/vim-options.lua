7fdf69dd9d06c0a95ee640b30efe7f974e45b3eb
DoDesCadeaux <dduraku@student.s19.be>
2024-04-26 20:03:45 +0200

    relative line nu + noh when insert

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set nu")
+vim.cmd("autocmd  InsertEnter * :set norelativenumber")
+vim.cmd("autocmd  InsertLeave * :set relativenumber")
+vim.cmd("autocmd  InsertEnter * :set nohlsearch")
 vim.g.mapleader = '\\'
 
 vim.cmd("set undofile")
